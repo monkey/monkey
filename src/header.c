bbc10cf70689dfb6bca1c1e1612e685852f94647
Eduardo Silva <edsiper@gmail.com>
2009-11-16 14:06:10 -0300

    HTTP: Close connections if KeepAlive is not specified

--- src/header.c
+++ src/header.c
@@ -210,36 +210,32 @@ int mk_header_send(int fd, struct client_request *cr,
 	/* Connection */
         if(config->keep_alive == VAR_ON){
                 /* Explicit request */
-                if(cr->request->connection.data != NULL){
-                        if(
-                           cr->request->keep_alive == VAR_ON &&
-                           (cr->counter_connections < 
-                            config->max_keep_alive_request)){
-
-                                m_build_buffer(
-                                               &buffer,
-                                               &len,
-                                               "Keep-Alive: timeout=%i, max=%i" 
-                                               MK_CRLF,
-                                               config->keep_alive_timeout, 
-                                               (config->max_keep_alive_request -
-                                                cr->counter_connections)
-                                               );
-                                mk_iov_add_entry(iov, buffer, len,
-                                                 mk_iov_none, MK_IOV_FREE_BUF);
-                                mk_iov_add_entry(iov,
-                                                 mk_header_conn_ka.data,
-                                                 mk_header_conn_ka.len,
-                                                 mk_iov_none, 
-                                                 MK_IOV_NOT_FREE_BUF);
-                        }
-                        else{
-                                mk_iov_add_entry(iov,
-                                                 mk_header_conn_close.data,
-                                                 mk_header_conn_close.len,
-                                                 mk_iov_none, 
-                                                 MK_IOV_NOT_FREE_BUF);
-                        }
+                if(cr->request->connection.data != NULL &&
+                   cr->request->keep_alive == VAR_ON &&
+                   (cr->counter_connections < config->max_keep_alive_request)){
+                        m_build_buffer(
+                                       &buffer,
+                                       &len,
+                                       "Keep-Alive: timeout=%i, max=%i" 
+                                       MK_CRLF,
+                                       config->keep_alive_timeout, 
+                                       (config->max_keep_alive_request -
+                                        cr->counter_connections)
+                                       );
+                        mk_iov_add_entry(iov, buffer, len,
+                                         mk_iov_none, MK_IOV_FREE_BUF);
+                        mk_iov_add_entry(iov,
+                                         mk_header_conn_ka.data,
+                                         mk_header_conn_ka.len,
+                                         mk_iov_none, 
+                                         MK_IOV_NOT_FREE_BUF);
+                }
+                else{
+                        mk_iov_add_entry(iov,
+                                         mk_header_conn_close.data,
+                                         mk_header_conn_close.len,
+                                         mk_iov_none, 
+                                         MK_IOV_NOT_FREE_BUF);
                 }
         }
 
