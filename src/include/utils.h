82957c6dc5669f9d5e0f133b055a73bc7bfd0368
Eduardo Silva <edsiper@gmail.com>
2010-04-19 17:20:18 -0400

    Cleanup utils.h

--- src/include/utils.h
+++ src/include/utils.h
@@ -60,30 +60,15 @@ char *envtrace;
 #endif
 
 /* utils.c */
-int AccessFile(struct stat file);
-int ExecFile(char *pathfile);
 int hex2int(char *pChars);
-char *strstr2(char *s, char *t);
 
 mk_pointer PutDate_string(time_t date);
-
 time_t PutDate_unix(char *date);
-
-char *get_name_protocol(int remote_protocol);
 
 char *m_build_buffer(char **buffer, unsigned long *len, const char *format,
                      ...);
 
 int mk_buffer_cat(mk_pointer * p, char *buf1, int len1, char *buf2, int len2);
-
-#define SYML_NOT -1
-#define SYML_OK 0
-#define SYML_VAR_OFF 1
-#define SYML_ERR_NOTFOUND 2
-#define SYML_ERR_FORBIDDEN 3
-
-int Check_symlink(const char *path);
-char *get_end_position(char *buf);
 
 int mk_utils_set_daemon();
 mk_pointer mk_utils_int2mkp(int n);
