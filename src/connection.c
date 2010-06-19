1603fdeea80376391df445a6edfe25bc906eb047
Eduardo Silva <edsiper@gmail.com>
2010-06-19 19:32:59 -0400

    Do not free CR before check KeepAlive

--- src/connection.c
+++ src/connection.c
@@ -140,8 +140,13 @@ int mk_conn_write(int socket)
         return -1;
     }
     else if (ret == 0) {
-        mk_request_free_list(cr);
-        return mk_http_request_end(socket);
+        if (mk_http_request_end(socket) < 0) {
+            mk_request_free_list(cr);
+            return -1;
+        }
+        else {
+            return 0;
+        }
     }
     else if (ret > 0) {
         return 0;
