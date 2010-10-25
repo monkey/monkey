87e1a2eecb4846c4a5250b1a3df4960b4778ffd0
Eduardo Silva <edsiper@gmail.com>
2010-10-25 15:20:56 -0300

    Fix mandril plugin

---
+++ plugins/mandril/mandril.h
@@ -0,0 +1,38 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+/*  Monkey HTTP Daemon
+ *  ------------------
+ *  Copyright (C) 2001-2009, Eduardo Silva P.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+/* security.c */
+#ifndef MK_SECURITY_H
+#define MK_SECURITY_H
+
+#define MK_SECURITY_TYPE_IP 0
+#define MK_SECURITY_TYPE_URL 1
+
+struct mk_security
+{
+    int type;
+    char *value;
+    struct mk_security *next;
+};
+
+struct mk_security *rules;
+
+#endif
