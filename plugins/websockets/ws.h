801f7737dbf823b56896a3245144d8d0821b2fda
Eduardo Silva <edsiper@gmail.com>
2011-11-03 23:36:44 -0300

    WebSockets: Initial plugin import
    Signed-off-by: Eduardo Silva <edsiper@gmail.com>

---
+++ plugins/websockets/ws.h
@@ -0,0 +1,52 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+/*  Monkey HTTP Daemon
+ *  ------------------
+ *  Copyright (C) 2001-2011, Eduardo Silva P.
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+#ifndef MK_WEBSOCKETS_H
+#define MK_WEBSOCKETS_H
+
+/* GUID is defined by websockets v10 */
+#define WS_GUID                    "258EAFA5-E914-47DA-95CA-C5AB0DC85B11"
+
+#define WS_PATH                    "/websockets"
+#define WS_CONN_UPGRADE            "Upgrade"
+#define WS_UPGRADE_WS              "websocket"
+
+/* Request headers */
+#define WS_HEADER_UPGRADE          "Upgrade:"
+#define WS_HEADER_SEC_WS_ORIGIN    "Sec-WebSocket-Origin:"
+#define WS_HEADER_SEC_WS_KEY       "Sec-WebSocket-Key:"
+#define WS_HEADER_SEC_WS_VERSION   "Sec-WebSocket-Version:"
+#define WS_HEADER_SEC_WS_PROTOCOL  "Sec-WebSocket-Protocol:"
+
+/* Response headers */
+#define WS_RESP_SWITCHING          "HTTP/1.1 101 Switching Protocols\r\n"
+#define WS_RESP_UPGRADE            "Upgrade: websocket"
+#define WS_RESP_CONNECTION         "Connection: Upgrade"
+#define WS_RESP_WS_ACCEPT          "Sec-WebSocket-Accept: "
+
+/* Framing macros */
+#define WS_FRAME_MASK_LEN          4
+
+#define CHECK_BIT(var, pos) !!((var) & (1 << (pos)))
+
+/* SHA1 stuff */
+#define SHA1_DIGEST_LEN            20
+#endif
