2a80e9f245a71a22bc08fa7e71b859df9951fc17
Eduardo Silva <edsiper@gmail.com>
2011-11-08 10:40:17 -0300

    Liana: do not print error when socket is unavailable
    Signed-off-by: Eduardo Silva <edsiper@gmail.com>

--- plugins/logger/logger.c
+++ plugins/logger/logger.c
@@ -437,6 +437,14 @@ int _mkp_stage_40(struct client_session *cs, struct session_request *sr)
     /* Format IP string */
     ip_str = mk_api->socket_ip_str(cs->socket, &len);
 
+    /* 
+     * If the socket is not longer available ip_str can be null, 
+     * so we must check this condition and return
+     */
+    if (!ip_str) {
+        return 0;
+    }
+
     /* Add IP to IOV */
     mk_api->iov_add_entry(iov, ip_str, len, mk_iov_none, MK_IOV_NOT_FREE_BUF);
     mk_api->iov_add_entry(iov, " - ", 3, mk_iov_none, MK_IOV_NOT_FREE_BUF);
