6287750ae1fe4c801c83fbc4175a61f0f6fbf557
Eduardo Silva <edsiper@gmail.com>
2012-02-16 19:52:06 -0600

    Duda: add example service
    Signed-off-by: Eduardo Silva <edsiper@gmail.com>

---
+++ plugins/duda/example/hello.c
@@ -0,0 +1,64 @@
+/* -*- Mode: C; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+
+#include "webservice.h"
+
+DUDA_REGISTER("Service Example", "service");
+
+/*
+ *
+ * URI Map example
+ * +--------------------------------------------------------------+
+ * |  Interface         Method     Param Name  Param Max Length   |
+ * +--------------------------------------------------------------+
+ * |  system           cpu_usage     cpu_id          5            |
+ * +--------------------------------------------------------------+
+ * |                   cpu_hz        cpu_id          5            |
+ * +--------------------------------------------------------------+
+ * |                   cpu_list                                   |                 
+ * +--------------------------------------------------------------+
+ *
+ */
+
+void *callback_cpu_usage()
+{
+
+}
+
+void *callback_cpu_hz()
+{
+
+}
+
+void *callback_cpu_list()
+{
+
+}
+
+int duda_map()
+{
+    duda_interface_t *if_system;
+    duda_method_t    *method;
+    duda_param_t     *param;
+
+    duda_map_init();
+
+    /* archive interface */
+    if_system = duda_interface_new("system");
+
+    /* /app/archive/list */
+    method = duda_method_new("cpu_usage", (void *) callback_cpu_usage, 1);
+    method_add_param(method, "cpu_id", 5);
+    interface_add_method(method, if_system);
+
+    method = duda_method_new("cpu_hz", (void *) callback_cpu_hz, 1);
+    method_add_param(method, "cpu_id", 5);
+    interface_add_method(method, if_system);
+
+    method = duda_method_new("cpu_list", (void *) callback_cpu_list, 0);
+    interface_add_method(method, if_system);
+
+    /* Add interface to map */
+    duda_map_add_interface(if_system);
+
+    duda_map_end();
+}
