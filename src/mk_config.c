dd092f94a0083dbbf6d423ccbeec750f8f544825
Lauri Kasanen <cand@gmx.com>
2012-03-23 13:06:57 -0600

    config: enable GNU extensions to get isblank()
    Signed-off-by: Eduardo Silva <edsiper@gmail.com>

--- src/mk_config.c
+++ src/mk_config.c
@@ -19,6 +19,9 @@
  *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 
+/* isblank is not in C89 */
+#define _GNU_SOURCE
+
 #include <dirent.h>
 #include <stdio.h>
 #include <sys/types.h>
