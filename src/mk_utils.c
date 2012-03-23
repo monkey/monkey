9a2b826e119513835ed65b63dbdf0bc78ab536fe
Lauri Kasanen <cand@gmx.com>
2012-03-23 16:42:52 -0600

    messages: only print colors to terminal
    Signed-off-by: Eduardo Silva <edsiper@gmail.com>

--- src/mk_utils.c
+++ src/mk_utils.c
@@ -299,6 +299,13 @@ void mk_utils_trace(const char *component, int color, const char *function,
     char *color_function = NULL;
     char *color_fileline = NULL;
 
+    char *reset_color = ANSI_RESET;
+    char *bold_color = ANSI_BOLD;
+    char *magenta_color = ANSI_MAGENTA;
+    char *red_color = ANSI_RED;
+    char *green_color = ANSI_GREEN;
+    char *cyan_color = ANSI_CYAN;
+
     struct timeval tv;
     struct timezone tz;
 
@@ -325,17 +332,29 @@ void mk_utils_trace(const char *component, int color, const char *function,
         break;
     }
 
+    /* Only print colors to a terminal */
+    if (!isatty(STDOUT_FILENO)) {
+        color_function = "";
+        color_fileline = "";
+        reset_color = "";
+        bold_color = "";
+        magenta_color = "";
+        red_color = "";
+        green_color = "";
+        cyan_color = "";
+    }
+
     va_start( args, format );
 
     printf("~ %s%2i.%i%s %s%s[%s%s%s%s%s|%s:%i%s] %s%s():%s ",
-           ANSI_CYAN, (int) (tv.tv_sec - monkey_init_time), (int) tv.tv_usec, ANSI_RESET,
-           ANSI_MAGENTA, ANSI_BOLD,
-           ANSI_RESET, ANSI_BOLD, ANSI_GREEN, component, color_fileline, file,
-           line, ANSI_MAGENTA,
-           color_function, function, ANSI_RED);
+           cyan_color, (int) (tv.tv_sec - monkey_init_time), (int) tv.tv_usec, reset_color,
+           magenta_color, bold_color,
+           reset_color, bold_color, green_color, component, color_fileline, file,
+           line, magenta_color,
+           color_function, function, red_color);
     vprintf(format, args );
     va_end(args);
-    printf("%s\n", ANSI_RESET);
+    printf("%s\n", reset_color);
     fflush(stdout);
 
     /* Mutex unlock */
@@ -450,6 +469,9 @@ void mk_print(int type, const char *format, ...)
 
     char *header_color = NULL;
     char *header_title = NULL;
+    char *bold_color = ANSI_BOLD;
+    char *reset_color = ANSI_RESET;
+    char *white_color = ANSI_WHITE;
     va_list args;
 
     va_start(args, format);
@@ -476,24 +498,32 @@ void mk_print(int type, const char *format, ...)
         break;
     }
 
+    /* Only print colors to a terminal */
+    if (!isatty(STDOUT_FILENO)) {
+        header_color = "";
+        bold_color = "";
+        reset_color = "";
+        white_color = "";
+    }
+
     now = time(NULL);
     current = localtime(&now);
     printf("%s[%s%i/%02i/%02i %02i:%02i:%02i%s]%s ",
-           ANSI_BOLD, ANSI_RESET,
+           bold_color, reset_color,
            current->tm_year + 1900,
            current->tm_mon + 1,
            current->tm_mday,
            current->tm_hour,
            current->tm_min,
            current->tm_sec,
-           ANSI_BOLD, ANSI_RESET);
+           bold_color, reset_color);
 
     printf("%s[%s%7s%s]%s ",
-           ANSI_BOLD, header_color, header_title, ANSI_WHITE, ANSI_RESET);
+           bold_color, header_color, header_title, white_color, reset_color);
 
     vprintf(format, args);
     va_end(args);
-    printf("%s\n", ANSI_RESET);
+    printf("%s\n", reset_color);
     fflush(stdout);
 }
 
