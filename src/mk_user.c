8a83a2645b51897b1cea99f1b73ee0b4f94f1a6f
Lauri Kasanen <cand@gmx.com>
2012-05-23 17:21:43 +0300

    user: Fix possible crash
    Fixes a Clang warning. If getpwnam returns NULL, accessing usr->something
    would segfault.
    
    Signed-off-by: Lauri Kasanen <cand@gmx.com>
    Signed-off-by: Eduardo Silva <edsiper@gmail.com>

--- src/mk_user.c
+++ src/mk_user.c
@@ -118,6 +118,7 @@ int mk_user_set_uidgid()
         /* Check if user exists  */
         if ((usr = getpwnam(config->user)) == NULL) {
             mk_err("Invalid user '%s'", config->user);
+            goto out;
         }
 
         if (initgroups(config->user, usr->pw_gid) != 0) {
@@ -136,6 +137,7 @@ int mk_user_set_uidgid()
         config->is_seteuid = MK_TRUE;
     }
 
+    out:
     EUID = geteuid();
     EGID = getegid();
 
