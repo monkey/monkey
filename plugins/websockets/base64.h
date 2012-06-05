87146aa7535061ff5e45ded5cd969a1eeb619de2
Eduardo Silva <edsiper@gmail.com>
2012-06-05 16:37:21 -0600

    Deprecate websockets and patas plugins
    Signed-off-by: Eduardo Silva <edsiper@gmail.com>

--- plugins/websockets/base64.h
+++
@@ -1,4 +0,0 @@
-unsigned char * base64_encode(const unsigned char *src, size_t len,
-			      size_t *out_len);
-unsigned char *base64_decode(const unsigned char *src, size_t len,
-                             size_t *out_len);
