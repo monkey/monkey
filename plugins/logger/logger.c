3c24fac36f7a09fd08b712c6cb6f5119e42af192
Lauri Kasanen <cand@gmx.com>
2012-06-08 14:44:55 +0300

    logger: Use correct printf formatter
    Signed-off-by: Lauri Kasanen <cand@gmx.com>
    Signed-off-by: Eduardo Silva <edsiper@gmail.com>

--- plugins/logger/logger.c
+++ plugins/logger/logger.c
@@ -222,7 +222,7 @@ void *mk_logger_worker_init(void *args)
             slen = splice(events[i].data.fd, NULL, flog,
                           NULL, bytes, SPLICE_F_MOVE);
             if (slen == -1) {
-                mk_warn("Could not write to log file: splice() = %i", slen);
+                mk_warn("Could not write to log file: splice() = %ld", slen);
             }
 
             PLUGIN_TRACE("written %i bytes", bytes);
