af633500a3cac9659052c3c344d00baca8bf6a9c
Eduardo Silva <edsiper@gmail.com>
2012-06-14 13:35:47 -0600

    Logger: adapt usage of worker_spawn()
    Signed-off-by: Eduardo Silva <edsiper@gmail.com>

--- plugins/logger/logger.c
+++ plugins/logger/logger.c
@@ -402,7 +402,7 @@ void _mkp_core_prctx()
         }
     }
 
-    mk_api->worker_spawn((void *) mk_logger_worker_init);
+    mk_api->worker_spawn((void *) mk_logger_worker_init, NULL);
 }
 
 void _mkp_core_thctx()
