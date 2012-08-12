fd0140287fad32d1ea53d9797a9f08bb815cb2f4
Sonny Karlsson <ksonny@lotrax.org>
2012-08-12 13:16:35 +0200

    fastcgi: Add experimental FastCGI proxy plugin.
    Signed-off-by: Sonny Karlsson <ksonny@lotrax.org>
    Signed-off-by: Eduardo Silva <edsiper@gmail.com>

---
+++ plugins/fastcgi/fcgi_env.h
@@ -0,0 +1,11 @@
+#ifndef _FCGI_ENV_H_
+#define _FCGI_ENV_H_
+
+#include <stdint.h>
+
+size_t fcgi_env_write(uint8_t *ptr,
+		const size_t len,
+		struct client_session *cs,
+		struct session_request *sr);
+
+#endif // _FCGI_ENV_H_
