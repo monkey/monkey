24b4c7eb8f30adcab0181787765caeffa30ad710
Vladimir Cernov <gg.kaspersky@gmail.com>
2014-06-07 23:18:55 +0300

    Libmonkey: Fix warning in md5_check.h
    Signed-off-by: Vladimir Cernov <gg.kaspersky@gmail.com>

--- tests/lib/md5_check.h
+++ tests/lib/md5_check.h
@@ -8,15 +8,14 @@
 
 int md5_check(const char *right, const char *where) {
 
-	char *def, buf[33];
-	asprintf(&def, "wget --timeout=2 -t2 -q -O- %s | md5sum -", where);
+	char def[1024], buf[33];
+	snprintf(def, 1024, "wget --timeout=2 -t2 -q -O- %s | md5sum -", where);
 
 	FILE *f = popen(def, "r");
 	if (!f) return 0;
 
 	fgets(buf, 33, f);
 
-	free(def);
 	pclose(f);
 
 	return (strcmp(right, buf) == 0);
