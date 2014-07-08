8cef79960bb32b00473d824c24ae854c10f466c3
Eduardo Silva <eduardo@monkey.io>
2014-07-07 23:02:51 -0600

    Jemalloc: upgrade to 3.6.0
    Signed-off-by: Eduardo Silva <eduardo@monkey.io>

--- deps/jemalloc/test/integration/allocm.c
+++ deps/jemalloc/test/integration/allocm.c
@@ -1,8 +1,7 @@
 #include "test/jemalloc_test.h"
 
 #define	CHUNK 0x400000
-/* #define MAXALIGN ((size_t)UINT64_C(0x80000000000)) */
-#define	MAXALIGN ((size_t)0x2000000LU)
+#define	MAXALIGN (((size_t)1) << 25)
 #define	NITER 4
 
 TEST_BEGIN(test_basic)
@@ -36,28 +35,6 @@ TEST_BEGIN(test_basic)
 	assert_zu_eq(nsz, rsz, "nallocm()/allocm() rsize mismatch");
 	assert_d_eq(dallocm(p, 0), ALLOCM_SUCCESS,
 	    "Unexpected dallocm() error");
-}
-TEST_END
-
-TEST_BEGIN(test_alignment_errors)
-{
-	void *p;
-	size_t nsz, rsz, sz, alignment;
-
-#if LG_SIZEOF_PTR == 3
-	alignment = UINT64_C(0x4000000000000000);
-	sz        = UINT64_C(0x8400000000000001);
-#else
-	alignment = 0x40000000LU;
-	sz        = 0x84000001LU;
-#endif
-	nsz = 0;
-	assert_d_eq(nallocm(&nsz, sz, ALLOCM_ALIGN(alignment)), ALLOCM_SUCCESS,
-	    "Unexpected nallocm() error");
-	rsz = 0;
-	assert_d_ne(allocm(&p, &rsz, sz, ALLOCM_ALIGN(alignment)),
-	    ALLOCM_SUCCESS, "Expected error for allocm(&p, %zu, %#x)",
-	    sz, ALLOCM_ALIGN(alignment));
 }
 TEST_END
 
@@ -126,6 +103,5 @@ main(void)
 
 	return (test(
 	    test_basic,
-	    test_alignment_errors,
 	    test_alignment_and_size));
 }
--- deps/jemalloc/test/unit/junk.c
+++ deps/jemalloc/test/unit/junk.c
@@ -73,7 +73,7 @@ test_junk(size_t sz_min, size_t sz_max)
 		if (sz_prev > 0) {
 			assert_c_eq(s[0], 'a',
 			    "Previously allocated byte %zu/%zu is corrupted",
-			    0, sz_prev);
+			    ZU(0), sz_prev);
 			assert_c_eq(s[sz_prev-1], 'a',
 			    "Previously allocated byte %zu/%zu is corrupted",
 			    sz_prev-1, sz_prev);
@@ -92,9 +92,12 @@ test_junk(size_t sz_min, size_t sz_max)
 			s = (char *)rallocx(s, sz+1, 0);
 			assert_ptr_not_null((void *)s,
 			    "Unexpected rallocx() failure");
-			assert_ptr_eq(most_recently_junked, junked,
-			    "Expected region of size %zu to be junk-filled",
-			    sz);
+			if (!config_mremap || sz+1 <= arena_maxclass) {
+				assert_ptr_eq(most_recently_junked, junked,
+				    "Expected region of size %zu to be "
+				    "junk-filled",
+				    sz);
+			}
 		}
 	}
 
