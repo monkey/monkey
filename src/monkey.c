d673a69a831643a006f3e33fe207dc3b714d505b
Eduardo Silva <eduardo@monkey.io>
2014-07-19 21:27:00 -0600

    Help: add missing break line on text
    Signed-off-by: Eduardo Silva <eduardo@monkey.io>

--- src/monkey.c
+++ src/monkey.c
@@ -108,7 +108,7 @@ static void mk_help(int rc)
     printf("  -m, --mimes-conf-file=FILE\t\tspecify mimes configuration file\n");
     printf("  -l, --plugins-load-conf-file=FILE\tspecify plugins.load configuration file\n");
     printf("  -S, --sites-conf-dir=dir\t\tspecify sites configuration directory\n");
-    printf("  -P, --plugins-conf-dir=dir\t\tspecify plugin configuration directory\n");
+    printf("  -P, --plugins-conf-dir=dir\t\tspecify plugin configuration directory\n\n");
     printf("%sInformational%s\n", ANSI_BOLD, ANSI_RESET);
     printf("  -b, --build\t\tprint build information\n");
     printf("  -v, --version\t\tshow version number\n");
