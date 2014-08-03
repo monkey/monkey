3c2c1fa4edf5860e03a5c606d4cce75319783e9f
kaspersky <gg.kaspersky@gmail.com>
2014-08-03 22:32:13 +0300

    Libmonkey: Move python extension module to appropriate location.
    Signed-off-by: kaspersky <gg.kaspersky@gmail.com>

--- lib/python/setup.py
+++ lib/python/setup.py
@@ -4,6 +4,10 @@
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
+import os
+
+os.environ['CFLAGS'] = '-I../../include/monkey'
+os.environ['LDFLAGS'] = '-L../../src'
 
 setup(ext_modules = cythonize(
     [
