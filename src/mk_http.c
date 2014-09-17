2c61531b8aa36477e84c8f03c8d3c3039fc4c925
Eduardo Silva <eduardo@monkey.io>
2014-09-16 22:23:00 -0600

    HTTP: Disable TCP_CORK on send_file on first round of data
    Signed-off-by: Eduardo Silva <eduardo@monkey.io>

--- src/mk_http.c
+++ src/mk_http.c
@@ -608,14 +608,18 @@ int mk_http_init(struct client_session *cs, struct session_request *sr)
 
 int mk_http_send_file(struct client_session *cs, struct session_request *sr)
 {
+    int first = MK_FALSE;
     long int nbytes = 0;
+
+    if (sr->bytes_offset == 0) {
+        first = MK_TRUE;
+    }
 
     nbytes = mk_socket_send_file(cs->socket, sr->fd_file,
                                  &sr->bytes_offset, sr->bytes_to_send);
-
     if (nbytes > 0) {
         sr->bytes_to_send -= nbytes;
-        if (sr->bytes_to_send == 0) {
+        if (first == MK_TRUE) {
             mk_server_cork_flag(cs->socket, TCP_CORK_OFF);
         }
     }
