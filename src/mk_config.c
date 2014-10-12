64e032b66d5f840f58d24e04875df8ddd9ef7c9b
Eduardo Silva <eduardo@monkey.io>
2014-10-11 23:09:01 -0600

    Config: on listener read, do not continue on loop
    Signed-off-by: Eduardo Silva <eduardo@monkey.io>

--- src/mk_config.c
+++ src/mk_config.c
@@ -486,9 +486,6 @@ static int mk_config_listen_read(struct mk_config_section *section)
 
         /* register the new listener */
         mk_config_listener_add(address, port);
-        mk_mem_free(address);
-        address = NULL;
-        continue;
 
 error:
         if (address) mk_mem_free(address);
