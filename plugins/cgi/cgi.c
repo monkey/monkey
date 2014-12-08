f7610d665032f445c570d242d30e2c58d8fb16b0
Eduardo Silva <eduardo@monkey.io>
2014-12-08 16:59:26 -0600

    CGI: lookup and set Cookie HTTP Header
    Signed-off-by: Eduardo Silva <eduardo@monkey.io>

--- plugins/cgi/cgi.c
+++ plugins/cgi/cgi.c
@@ -68,6 +68,7 @@ static int do_cgi(const char *const __restrict__ file,
     int ret;
     const int socket = cs->socket;
     struct file_info finfo;
+    mk_ptr_t header;
 
     char *env[30];
 
@@ -95,6 +96,7 @@ static int do_cgi(const char *const __restrict__ file,
     char server_software[SHORTLEN];
     char server_protocol[SHORTLEN];
     char http_host[SHORTLEN];
+    char cookie[512];
 
     /* Check the interpreter exists */
     if (match->bin) {
@@ -166,6 +168,12 @@ static int do_cgi(const char *const __restrict__ file,
         env[envpos++] = content_type;
     }
 
+    header = mk_api->header_get(&sr->headers_toc, "COOKIE", 6);
+    if (header.len > 0) {
+        snprintf(cookie, sizeof(cookie) - 1,
+                 "HTTP_COOKIE=%.*s", (int) header.len - 1, header.data + 1);
+        env[envpos++] = cookie;
+    }
 
     /* Must be NULL-terminated */
     env[envpos] = NULL;
