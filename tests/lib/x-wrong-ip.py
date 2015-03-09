69511b932f1120e867dea14b519681d06ded99d1
Eduardo Silva <eduardo@monkey.io>
2015-03-08 19:00:44 -0600

    Lib: deprecate library implementation.
    This patch removes the library mode as in the next changes Monkey
    will become a library it self.
    
    Signed-off-by: Eduardo Silva <eduardo@monkey.io>

--- tests/lib/x-wrong-ip.py
+++
@@ -1,5 +0,0 @@
-import monkey
-import os
-
-if not monkey.init('lol', 0, 0, None):
-    os._exit(1)
--- tests/lib/x-wrong-port.py
+++
@@ -1,5 +0,0 @@
-import monkey
-import os
-
-if not monkey.init(None, -1, 0, None):
-    os._exit(1)
