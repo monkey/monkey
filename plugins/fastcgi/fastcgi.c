b773e8188841a92789a6deac2c38b7300296a9c9
Eduardo Silva <eduardo@monkey.io>
2016-08-14 20:42:14 -0600

    Plugins: FastCGI: fix exit conditionals
    Signed-off-by: Eduardo Silva <eduardo@monkey.io>

--- plugins/fastcgi/fastcgi.c
+++ plugins/fastcgi/fastcgi.c
@@ -38,9 +38,11 @@ static int mk_fastcgi_config(char *path)
 
     mk_api->str_build(&file, &len, "%sfastcgi.conf", path);
     conf = mk_api->config_create(file);
+    mk_mem_free(file);
     if (!conf) {
         return -1;
     }
+
     section = mk_api->config_section_get(conf, "FASTCGI_SERVER");
     if (!section) {
         return -1;
@@ -151,6 +153,8 @@ int mk_fastcgi_stage30_hangup(struct mk_plugin *plugin,
     }
 
     handler->active = MK_FALSE;
+    handler->hangup = MK_TRUE;
+
     fcgi_exit(sr->handler_data);
 
     return 0;
