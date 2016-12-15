e939463fe3ef383c7021e49169e1778cc3332eb2
Eduardo Silva <eduardo@monkey.io>
2016-12-15 11:33:26 -0600

    core: memory: remove unused iov header
    Signed-off-by: Eduardo Silva <eduardo@monkey.io>

--- mk_core/mk_memory.c
+++ mk_core/mk_memory.c
@@ -24,7 +24,6 @@
 
 #include <mk_core/mk_pthread.h>
 #include <mk_core/mk_memory.h>
-#include <mk_core/mk_iov.h>
 #include <mk_core/mk_macros.h>
 
 mk_ptr_t mk_ptr_create(char *buf, long init, long end)
