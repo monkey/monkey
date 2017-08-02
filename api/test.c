c937b4014f7b679e3ad749d94d88a9b1e2c1acd9
Eduardo Silva <eduardo@monkey.io>
2017-08-02 15:26:07 -0600

    lib: update API test
    Signed-off-by: Eduardo Silva <eduardo@monkey.io>

--- api/test.c
+++ api/test.c
@@ -11,23 +11,38 @@
 
 void cb_worker(void *data)
 {
-    printf("test worker callback; data=%p\n", data);
+    mk_info("[api test] test worker callback; data=%p", data);
 }
 
-void cb_main(mk_request_t *request, void *data)
+void cb_test_chunks(mk_request_t *request, void *data)
 {
-    int i;
-    char *buf = "this is a test\n";
-    int len = 15;
+    int i = 0;
+    int len;
+    char tmp[32];
     (void) data;
 
     mk_http_status(request, 200);
     mk_http_header(request, "X-Monkey", 8, "OK", 2);
 
-    for (i = 0; i < 10; i++) {
-        mk_http_send(request, buf, len, NULL);
+    for (i = 0; i < 1000000; i++) {
+        len = snprintf(tmp, sizeof(tmp) -1, "test-chunk %6i\n ", i);
+        mk_http_send(request, tmp, len, NULL);
     }
+    mk_http_done(request);
+}
 
+void cb_test_big_chunk(mk_request_t *request, void *data)
+{
+    size_t chunk_size = 1024000000;
+    char *chunk;
+    (void) data;
+
+    mk_http_status(request, 200);
+    mk_http_header(request, "X-Monkey", 8, "OK", 2);
+
+    chunk = calloc(1, chunk_size);
+    mk_http_send(request, chunk, chunk_size, NULL);
+    free(chunk);
     mk_http_done(request);
 }
 
@@ -45,7 +60,8 @@ int main()
     mk_vhost_set(ctx, vid,
                  "Name", "monotop",
                  NULL);
-    mk_vhost_handler(ctx, vid, "/test", cb_main, NULL);
+    mk_vhost_handler(ctx, vid, "/test_chunks", cb_test_chunks, NULL);
+    mk_vhost_handler(ctx, vid, "/test_big_chunk", cb_test_big_chunk, NULL);
 
     mk_worker_callback(ctx,
                        cb_worker,
@@ -54,7 +70,7 @@ int main()
     mk_info("Service: http://%s:%s/test",  API_ADDR, API_PORT);
     mk_start(ctx);
 
-    sleep(60);
+    sleep(6000);
 
     mk_stop(ctx);
 
