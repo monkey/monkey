monkey/monkey

commit c1c0445f6214fd6cc74ec6a530585f9130bc65c2

server/lib: get rid of mk_config global context
Signed-off-by: Eduardo Silva <eduardo@monkey.io>

include/monkey/mk_config.h
@@ -53,7 +53,7 @@ struct mk_config_listener
 };
 
 /* Base struct of server */
-struct mk_server_config
+struct mk_server
 {
     int server_fd;                /* server socket file descriptor */
     int kernel_version;           /* Running Linux Kernel version */
@@ -155,30 +155,25 @@ struct mk_server_config
     struct mk_list stage50_handler;
 };
 
-/* Global context of the server */
-struct mk_server_config *mk_config;
-
-
 /* Functions */
 struct mk_server_config *mk_config_init();
-void mk_config_start_configure(void);
-void mk_config_signature(struct mk_server_config *config);
+void mk_config_start_configure(struct mk_server *server);
+void mk_config_signature(struct mk_server *server);
 void mk_config_add_index(char *indexname);
-void mk_config_set_init_values(struct mk_server_config *config);
-int mk_config_listen_parse(char *value);
+void mk_config_set_init_values(struct mk_server *config);
+int mk_config_listen_parse(char *value, struct mk_server *server);
 
 /* config helpers */
 void mk_config_error(const char *path, int line, const char *msg);
-
-struct mk_config_listener *mk_config_listener_add(char *address, char *port,
-                                                  int flags);
-
+struct mk_config_listener *mk_config_listener_add(char *address,
+                                                  char *port, int flags,
+                                                  struct mk_server *server);
 int mk_config_listen_check_busy();
 void mk_config_listeners_free();
 
 int mk_config_get_bool(char *value);
 void mk_config_read_hosts(char *path);
-void mk_config_sanity_check(void);
-void mk_config_free_all();
+void mk_config_sanity_check(struct mk_server *server);
+void mk_config_free_all(struct mk_server *server);
 
 #endif

include/monkey/mk_plugin.h
@@ -98,7 +98,7 @@ struct plugin_api
     int (*socket_send_file) (int, int, off_t *, size_t);
     int (*socket_ip_str) (int, char **, int, unsigned long *);
 
-    struct mk_server_config *config;
+    struct mk_server *config;
     struct mk_list *plugins;
     struct mk_sched_worker *sched_list;
 
@@ -106,8 +106,10 @@ struct plugin_api
     void (*_error) (int, const char *, ...) PRINTF_WARNINGS(2,3);
 
     /* HTTP request function */
-    int   (*http_request_end) (struct mk_http_session *cs, int close);
-    int   (*http_request_error) (int, struct mk_http_session *, struct mk_http_request *);
+    int   (*http_request_end) (struct mk_http_session *cs, int close,
+                               struct mk_server *);
+    int   (*http_request_error) (int, struct mk_http_session *,
+                                 struct mk_http_request *, struct mk_server *);
 
     /* memory functions */
     void *(*mem_alloc) (const size_t size);
@@ -136,7 +138,8 @@ struct plugin_api
 
     /* header */
     int  (*header_prepare) (struct mk_http_session *,
-                            struct mk_http_request *);
+                            struct mk_http_request *,
+                            struct mk_server *);
     struct mk_http_header *(*header_get) (int, struct mk_http_request *,
                                           const char *, unsigned int);
     int  (*header_add) (struct mk_http_request *, char *row, int len);
@@ -199,7 +202,7 @@ struct plugin_api
 
     /* Scheduler */
     struct mk_event_loop *(*sched_loop)();
-    int (*sched_remove_client) (int);
+    int (*sched_remove_client) (int, struct mk_server *);
     struct mk_sched_conn *(*sched_get_connection)(struct mk_sched_worker *,
                                                   int);
     void (*sched_event_free) (struct mk_event *);
@@ -229,7 +232,7 @@ struct plugin_api
 
     /* kernel interfaces */
     int (*kernel_version) ();
-    int (*kernel_features_print) (char *, size_t);
+    int (*kernel_features_print) (char *, size_t, struct mk_server *);
 
     /* Handler */
     struct mk_handler_param *(*handler_param_get)(int, struct mk_list *);
@@ -265,7 +268,7 @@ struct mk_plugin {
     int (*exit_plugin) ();
 
     /* Init Levels */
-    int  (*master_init) (struct mk_server_config *);
+    int  (*master_init) (struct mk_server *);
     void (*worker_init) ();
 
     /* Callback references for plugin type */
@@ -305,7 +308,7 @@ struct mk_plugin_stage {
 
 void mk_plugin_api_init();
 void mk_plugin_load_all();
-void mk_plugin_exit_all();
+void mk_plugin_exit_all(struct mk_server *server);
 void mk_plugin_exit_worker();
 
 void mk_plugin_event_init_list();
@@ -315,10 +318,10 @@ int mk_plugin_stage_run(unsigned int stage,
                         struct mk_sched_conn *conx,
                         struct mk_http_session *cs, struct mk_http_request *sr);
 
-void mk_plugin_core_process();
+void mk_plugin_core_process(struct mk_server *server);
 void mk_plugin_core_thread();
 
-void mk_plugin_preworker_calls();
+void mk_plugin_preworker_calls(struct mk_server *server);
 
 /* Plugins events interface */
 int mk_plugin_event_add(int socket, int mode,
@@ -330,9 +333,11 @@ struct plugin_event *mk_plugin_event_get(int socket);
 int mk_plugin_event_socket_change_mode(int socket, int mode, unsigned int behavior);
 
 struct mk_plugin *mk_plugin_load(int type, const char *shortname,
-                                 void *data);
+                                 void *data, struct mk_server *server);
+
 void *mk_plugin_load_symbol(void *handler, const char *symbol);
-int mk_plugin_http_request_end(struct mk_http_session *cs, int close);
+int mk_plugin_http_request_end(struct mk_http_session *cs, int close,
+                               struct mk_server *server);
 
 /* Register functions */
 struct plugin *mk_plugin_register(struct plugin *p);
@@ -344,7 +349,8 @@ void mk_plugin_free(struct mk_plugin *p);
 int mk_plugin_time_now_unix();
 mk_ptr_t *mk_plugin_time_now_human();
 
-int mk_plugin_sched_remove_client(int socket);
+int mk_plugin_sched_remove_client(int socket, struct mk_server *server);
+
 
 int mk_plugin_header_add(struct mk_http_request *sr, char *row, int len);
 int mk_plugin_header_get(struct mk_http_request *sr,
@@ -352,10 +358,10 @@ int mk_plugin_header_get(struct mk_http_request *sr,
                          mk_ptr_t *result);
 
 struct mk_sched_worker *mk_plugin_sched_get_thread_conf();
-struct mk_plugin *mk_plugin_cap(char cap, struct mk_server_config *config);
-struct mk_plugin *mk_plugin_lookup(char *shortname);
+struct mk_plugin *mk_plugin_cap(char cap, struct mk_server *server);
+struct mk_plugin *mk_plugin_lookup(char *shortname, struct mk_server *server);
 
-void mk_plugin_load_static();
+void mk_plugin_load_static(struct mk_server *server);
 struct mk_handler_param *mk_handler_param_get(int id, struct mk_list *params);
 
 #endif

mk_server/mk_plugin.c
@@ -39,12 +39,12 @@ static struct plugin_stagemap *plg_stagemap;
 struct plugin_network_io *plg_netiomap;
 struct plugin_api *api;
 
-struct mk_plugin *mk_plugin_lookup(char *shortname)
+struct mk_plugin *mk_plugin_lookup(char *shortname, struct mk_server *server)
 {
     struct mk_list *head;
     struct mk_plugin *p = NULL;
 
-    mk_list_foreach(head, &mk_config->plugins) {
+    mk_list_foreach(head, &server->plugins) {
         p = mk_list_entry(head, struct mk_plugin, _head);
         if (strcmp(p->shortname, shortname) == 0){
             return p;
@@ -80,7 +80,8 @@ void *mk_plugin_load_symbol(void *handler, const char *symbol)
 }
 
 /* Initialize a plugin, trigger the init_plugin callback */
-static int mk_plugin_init(struct plugin_api *api, struct mk_plugin *plugin)
+static int mk_plugin_init(struct plugin_api *api, struct mk_plugin *plugin,
+                          struct mk_server *server)
 {
     int ret;
     unsigned long len;
@@ -91,10 +92,10 @@ static int mk_plugin_init(struct plugin_api *api, struct mk_plugin *plugin)
     MK_TRACE("Load Plugin: '%s'", plugin->shortname);
 
     snprintf(path, 1024, "%s/%s",
-             mk_config->path_conf_root, mk_config->conf_plugins);
+             server->path_conf_root, server->conf_plugins);
     ret = mk_file_get_info(path, &f_info, MK_FILE_READ);
     if (ret == -1 || f_info.is_directory == MK_FALSE) {
-        snprintf(path, 1024, "%s", mk_config->conf_plugins);
+        snprintf(path, 1024, "%s", server->conf_plugins);
     }
 
     /* Build plugin configuration path */
@@ -118,7 +119,7 @@ static int mk_plugin_init(struct plugin_api *api, struct mk_plugin *plugin)
  * library.
  */
 struct mk_plugin *mk_plugin_load(int type, const char *shortname,
-                                 void *data)
+                                 void *data, struct mk_server *server)
 {
     char *path;
     char symbol[64];
@@ -145,7 +146,7 @@ struct mk_plugin *mk_plugin_load(int type, const char *shortname,
         }
 
         /* Make sure this is not loaded twice (ref #218) */
-        mk_list_foreach(head, &mk_config->plugins) {
+        mk_list_foreach(head, &server->plugins) {
             tmp = mk_list_entry(head, struct mk_plugin, _head);
             if (tmp->load_type == MK_PLUGIN_STATIC &&
                 strcmp(tmp->name, plugin->name) == 0){
@@ -190,42 +191,42 @@ struct mk_plugin *mk_plugin_load(int type, const char *shortname,
             st = mk_mem_alloc(sizeof(struct mk_plugin_stage));
             st->stage10 = stage->stage10;
             st->plugin  = plugin;
-            mk_list_add(&st->_head, &mk_config->stage10_handler);
+            mk_list_add(&st->_head, &server->stage10_handler);
             mk_list_add(&st->_parent_head, &plugin->stage_list);
         }
         if (stage->stage20) {
             st = mk_mem_alloc(sizeof(struct mk_plugin_stage));
             st->stage20 = stage->stage20;
             st->plugin  = plugin;
-            mk_list_add(&st->_head, &mk_config->stage20_handler);
+            mk_list_add(&st->_head, &server->stage20_handler);
             mk_list_add(&st->_parent_head, &plugin->stage_list);
         }
         if (stage->stage30) {
             st = mk_mem_alloc(sizeof(struct mk_plugin_stage));
             st->stage30 = stage->stage30;
             st->plugin  = plugin;
-            mk_list_add(&st->_head, &mk_config->stage30_handler);
+            mk_list_add(&st->_head, &server->stage30_handler);
             mk_list_add(&st->_parent_head, &plugin->stage_list);
         }
         if (stage->stage40) {
             st = mk_mem_alloc(sizeof(struct mk_plugin_stage));
             st->stage40 = stage->stage40;
             st->plugin  = plugin;
-            mk_list_add(&st->_head, &mk_config->stage40_handler);
+            mk_list_add(&st->_head, &server->stage40_handler);
             mk_list_add(&st->_parent_head, &plugin->stage_list);
         }
         if (stage->stage50) {
             st = mk_mem_alloc(sizeof(struct mk_plugin_stage));
             st->stage50 = stage->stage50;
             st->plugin  = plugin;
-            mk_list_add(&st->_head, &mk_config->stage50_handler);
+            mk_list_add(&st->_head, &server->stage50_handler);
             mk_list_add(&st->_parent_head, &plugin->stage_list);
         }
     }
 
     if (type == MK_PLUGIN_DYNAMIC) {
         /* Add Plugin to the end of the list */
-        mk_list_add(&plugin->_head, &mk_config->plugins);
+        mk_list_add(&plugin->_head, &server->plugins);
     }
 
     return plugin;
@@ -241,14 +242,14 @@ void mk_plugin_unregister(struct mk_plugin *p)
 
 }
 
-void mk_plugin_api_init()
+void mk_plugin_api_init(struct mk_server *server)
 {
     /* Create an instance of the API */
     api = mk_mem_alloc_z(sizeof(struct plugin_api));
     __builtin_prefetch(api);
 
     /* Setup and connections list */
-    api->config = mk_config;
+    api->config = server;
     api->sched_list = sched_list;
 
     /* API plugins funcions */
@@ -371,19 +372,20 @@ void mk_plugin_api_init()
     api->stacktrace = (void *) mk_utils_stacktrace;
     api->kernel_version = mk_kernel_version;
     api->kernel_features_print = mk_kernel_features_print;
-    api->plugins = &mk_config->plugins;
+    api->plugins = &server->plugins;
 
     /* handler */
     api->handler_param_get = mk_handler_param_get;
 }
 
-void mk_plugin_load_static()
+void mk_plugin_load_static(struct mk_server *server)
 {
     /* Load static plugins */
-    mk_static_plugins();
+    mk_list_init(&server->plugins);
+    mk_static_plugins(&server->plugins);
 }
 
-void mk_plugin_load_all()
+void mk_plugin_load_all(struct mk_server *server)
 {
     int ret;
     char *tmp;
@@ -397,18 +399,19 @@ void mk_plugin_load_all()
     struct mk_list *htmp;
     struct file_info f_info;
 
-    mk_plugin_load_static();
-    mk_list_foreach_safe(head, htmp, &mk_config->plugins) {
+    mk_plugin_load_static(server);
+    mk_list_foreach_safe(head, htmp, &server->plugins) {
         p = mk_list_entry(head, struct mk_plugin, _head);
 
         /* Load the static plugin */
         p = mk_plugin_load(MK_PLUGIN_STATIC,
                            p->shortname,
-                           (void *) p);
+                           (void *) p,
+                           server);
         if (!p) {
             continue;
         }
-        ret = mk_plugin_init(api, p);
+        ret = mk_plugin_init(api, p, server);
         if (ret == -1) {
             /* Free plugin, do not register, error initializing */
             mk_warn("Plugin initialization failed: %s", p->shortname);
@@ -423,17 +426,17 @@ void mk_plugin_load_all()
     }
 
     /* In case there are not dynamic plugins */
-    if (!mk_config->conf_plugin_load) {
+    if (!server->conf_plugin_load) {
         return;
     }
 
     /* Read configuration file */
     path = mk_mem_alloc_z(1024);
-    snprintf(path, 1024, "%s/%s", mk_config->path_conf_root,
-             mk_config->conf_plugin_load);
+    snprintf(path, 1024, "%s/%s", server->path_conf_root,
+             server->conf_plugin_load);
     ret = mk_file_get_info(path, &f_info, MK_FILE_READ);
     if (ret == -1 || f_info.is_file == MK_FALSE) {
-        snprintf(path, 1024, "%s", mk_config->conf_plugin_load);
+        snprintf(path, 1024, "%s", server->conf_plugin_load);
     }
 
     cnf = mk_rconf_open(path);
@@ -463,13 +466,14 @@ void mk_plugin_load_all()
             /* Load the dynamic plugin */
             p = mk_plugin_load(MK_PLUGIN_DYNAMIC,
                                shortname,
-                               entry->val);
+                               entry->val,
+                               server);
             if (!p) {
                 mk_warn("Invalid plugin '%s'", entry->val);
                 continue;
             }
 
-            ret = mk_plugin_init(api, p);
+            ret = mk_plugin_init(api, p, server);
             if (ret < 0) {
                 /* Free plugin, do not register */
                 MK_TRACE("Unregister plugin '%s'", p->shortname);
@@ -480,8 +484,8 @@ void mk_plugin_load_all()
     }
 
     /* Look for plugins thread key data */
-    mk_plugin_preworker_calls();
-    mk_vhost_map_handlers();
+    mk_plugin_preworker_calls(server);
+    mk_vhost_map_handlers(server);
     mk_mem_free(path);
     mk_rconf_free(cnf);
 }
@@ -505,19 +509,19 @@ static void mk_plugin_exit_stages(struct mk_plugin *p)
 }
 
 /* Invoke all plugins 'exit' hook and free resources by the plugin interface */
-void mk_plugin_exit_all()
+void mk_plugin_exit_all(struct mk_server *server)
 {
     struct mk_plugin *plugin;
     struct mk_list *head, *tmp;
 
     /* Plugins */
-    mk_list_foreach(head, &mk_config->plugins) {
+    mk_list_foreach(head, &server->plugins) {
         plugin = mk_list_entry(head, struct mk_plugin, _head);
         plugin->exit_plugin();
     }
 
     /* Plugin interface it self */
-    mk_list_foreach_safe(head, tmp, &mk_config->plugins) {
+    mk_list_foreach_safe(head, tmp, &server->plugins) {
         plugin = mk_list_entry(head, struct mk_plugin, _head);
         mk_list_del(&plugin->_head);
         mk_plugin_exit_stages(plugin);
@@ -543,17 +547,17 @@ void mk_plugin_exit_worker()
  * for plugins which need to set some data under a thread
  * context
  */
-void mk_plugin_core_process()
+void mk_plugin_core_process(struct mk_server *server)
 {
     struct mk_plugin *node;
     struct mk_list *head;
 
-    mk_list_foreach(head, &mk_config->plugins) {
+    mk_list_foreach(head, &server->plugins) {
         node = mk_list_entry(head, struct mk_plugin, _head);
 
         /* Init plugin */
         if (node->master_init) {
-            node->master_init(mk_config);
+            node->master_init(server);
         }
     }
 }
@@ -562,18 +566,18 @@ void mk_plugin_core_process()
  * for plugins which need to set some data under a thread
  * context
  */
-void mk_plugin_core_thread()
+void mk_plugin_core_thread(struct mk_server *server)
 {
 
     struct mk_plugin *node;
     struct mk_list *head;
 
-    mk_list_foreach(head, &mk_config->plugins) {
+    mk_list_foreach(head, &server->plugins) {
         node = mk_list_entry(head, struct mk_plugin, _head);
 
         /* Init plugin thread context */
         if (node->worker_init) {
-            node->worker_init();
+            node->worker_init(server);
         }
     }
 }
@@ -582,13 +586,13 @@ void mk_plugin_core_thread()
  * thread context for plugins, so here's the right
  * place to set pthread keys or similar
  */
-void mk_plugin_preworker_calls()
+void mk_plugin_preworker_calls(struct mk_server *server)
 {
     int ret;
     struct mk_plugin *node;
     struct mk_list *head;
 
-    mk_list_foreach(head, &mk_config->plugins) {
+    mk_list_foreach(head, &server->plugins) {
         node = mk_list_entry(head, struct mk_plugin, _head);
 
         /* Init pthread keys */
@@ -604,7 +608,8 @@ void mk_plugin_preworker_calls()
     }
 }
 
-int mk_plugin_http_request_end(struct mk_http_session *cs, int close)
+int mk_plugin_http_request_end(struct mk_http_session *cs, int close,
+                               struct mk_server *server)
 {
     int ret;
     int con;
@@ -619,18 +624,18 @@ int mk_plugin_http_request_end(struct mk_http_session *cs, int close)
     }
 
     sr = mk_list_entry_last(&cs->request_list, struct mk_http_request, _head);
-    mk_plugin_stage_run_40(cs, sr);
+    mk_plugin_stage_run_40(cs, sr, server);
 
     if (close == MK_TRUE) {
         cs->close_now = MK_TRUE;
     }
 
     /* Let's check if we should ask to finalize the connection or not */
-    ret = mk_http_request_end(cs);
+    ret = mk_http_request_end(cs, server);
     MK_TRACE("[FD %i] HTTP session end = %i", cs->socket, ret);
     if (ret < 0) {
         con = mk_sched_event_close(cs->conn, mk_sched_get_thread_conf(),
-                                   MK_EP_SOCKET_DONE);
+                                   MK_EP_SOCKET_DONE, server);
         if (con != 0) {
             return con;
         }
@@ -667,7 +672,7 @@ mk_ptr_t *mk_plugin_time_now_human()
     return &log_current_time;
 }
 
-int mk_plugin_sched_remove_client(int socket)
+int mk_plugin_sched_remove_client(int socket, struct mk_server *server)
 {
     struct mk_sched_conn *conn;
     struct mk_sched_worker *sched;
@@ -680,7 +685,7 @@ int mk_plugin_sched_remove_client(int socket)
         return -1;
     }
 
-    return mk_sched_remove_client(conn, sched);
+    return mk_sched_remove_client(conn, sched, server);
 }
 
 int mk_plugin_header_add(struct mk_http_request *sr, char *row, int len)
@@ -712,12 +717,12 @@ struct mk_sched_worker *mk_plugin_sched_get_thread_conf()
     return MK_TLS_GET(mk_tls_sched_worker_node);
 }
 
-struct mk_plugin *mk_plugin_cap(char cap, struct mk_server_config *config)
+struct mk_plugin *mk_plugin_cap(char cap, struct mk_server *server)
 {
     struct mk_list *head;
     struct mk_plugin *plugin;
 
-    mk_list_foreach(head, &config->plugins) {
+    mk_list_foreach(head, &server->plugins) {
         plugin = mk_list_entry(head, struct mk_plugin, _head);
         if (plugin->capabilities & cap) {
             return plugin;

CMakeLists.txt
@@ -308,6 +308,7 @@ add_subdirectory(man)
 add_subdirectory(mk_core)
 add_subdirectory(plugins)
 add_subdirectory(mk_server)
+
 if(NOT MK_WITHOUT_BIN)
   add_subdirectory(mk_bin)
 endif()

include/CMakeLists.txt
@@ -1,4 +1,4 @@
-if(NOT BUILD_LOCAL)
+if(NOT MK_LOCAL)
   install(DIRECTORY
       ${CMAKE_CURRENT_SOURCE_DIR}/monkey/
     DESTINATION

include/monkey/mk_core/mk_utils.h
@@ -41,6 +41,11 @@ pthread_mutex_t mutex_trace;
                                      __FUNCTION__, __FILENAME__, __LINE__, __VA_ARGS__)
 
 #else
+
+#ifdef MK_TRACE
+#undef MK_TRACE
+#endif
+
 #define MK_TRACE(...) do {} while (0)
 #endif
 

include/monkey/mk_header.h
@@ -103,7 +103,9 @@ extern const mk_ptr_t mk_header_accept_ranges;
 extern const mk_ptr_t mk_header_te_chunked;
 extern const mk_ptr_t mk_header_last_modified;
 
-int mk_header_prepare(struct mk_http_session *cs, struct mk_http_request *sr);
+int mk_header_prepare(struct mk_http_session *cs, struct mk_http_request *sr,
+                      struct mk_server *server);
+
 void mk_header_response_reset(struct response_headers *header);
 void mk_header_set_http_status(struct mk_http_request *sr, int status);
 void mk_header_set_content_length(struct mk_http_request *sr, long len);

include/monkey/mk_http.h
@@ -173,7 +173,8 @@ static inline int mk_http_status_completed(struct mk_http_session *cs,
 }
 
 int mk_http_error(int http_status, struct mk_http_session *cs,
-                      struct mk_http_request *sr);
+                  struct mk_http_request *sr,
+                  struct mk_server *server);
 
 int mk_http_method_check(mk_ptr_t method);
 mk_ptr_t mk_http_method_check_str(int method);
@@ -182,9 +183,12 @@ int mk_http_method_get(char *body);
 int mk_http_protocol_check(char *protocol, int len);
 mk_ptr_t mk_http_protocol_check_str(int protocol);
 
-int mk_http_init(struct mk_http_session *cs, struct mk_http_request *sr);
+int mk_http_init(struct mk_http_session *cs, struct mk_http_request *sr,
+                 struct mk_server *server);
+
 int mk_http_keepalive_check(struct mk_http_session *cs,
-                            struct mk_http_request *sr);
+                            struct mk_http_request *sr,
+                            struct mk_server *server);
 
 int mk_http_pending_request(struct mk_http_session *cs);
 int mk_http_send_file(struct mk_http_session *cs, struct mk_http_request *sr);
@@ -192,20 +196,26 @@ int mk_http_send_file(struct mk_http_session *cs, struct mk_http_request *sr);
 /* http session */
 int mk_http_session_init(struct mk_http_session *cs,
                          struct mk_sched_conn *conn);
-void mk_http_session_remove(struct mk_http_session *cs);
+void mk_http_session_remove(struct mk_http_session *cs,
+                            struct mk_server *server);
 
 /* event handlers */
-int mk_http_handler_read(struct mk_sched_conn *conn, struct mk_http_session *cs);
+int mk_http_handler_read(struct mk_sched_conn *conn, struct mk_http_session *cs,
+                         struct mk_server *server);
+
 int mk_http_handler_write(int socket, struct mk_http_session *cs);
 
-void mk_http_request_free(struct mk_http_request *sr);
-void mk_http_request_free_list(struct mk_http_session *cs);
+void mk_http_request_free(struct mk_http_request *sr, struct mk_server *server);
+void mk_http_request_free_list(struct mk_http_session *cs,
+                               struct mk_server *server);
 
 void mk_http_request_init(struct mk_http_session *session,
-                          struct mk_http_request *request);
+                          struct mk_http_request *request,
+                          struct mk_server *server);
 struct mk_http_header *mk_http_header_get(int name, struct mk_http_request *req,
                                           const char *key, unsigned int len);
-int mk_http_request_end(struct mk_http_session *cs);
+
+int mk_http_request_end(struct mk_http_session *cs, struct mk_server *server);
 
 #define mk_http_session_get(conn)               \
     (struct mk_http_session *)                  \

commit 64e2d325fa108044693141e09609efde56c01e3e

plugin: auth: Fix building error
This error might be introduced in:
c1c0445f6214fd6cc74ec6a530585f9130bc65c2

Signed-off-by: Hiroshi Hatake <hatake@calyptia.com>

include/monkey/mk_config.h
@@ -194,7 +194,6 @@ struct mk_server
 };
 
 /* Functions */
-struct mk_server_config *mk_config_init();
 void mk_config_start_configure(struct mk_server *server);
 void mk_config_signature(struct mk_server *server);
 void mk_config_add_index(char *indexname);

include/monkey/mk_plugin.h
@@ -104,7 +104,7 @@ struct plugin_api
     /* Async Network */
     struct mk_net_connection *(*net_conn_create) (char *, int);
 
-    struct mk_server_config *config;
+    struct mk_list *hosts;
     struct mk_list *plugins;
 
     /* Error helper */

mk_server/mk_plugin.c
@@ -277,7 +277,7 @@ void mk_plugin_api_init(struct mk_server *server)
 #endif
 
     /* Setup and connections list */
-    /* FIXME: api->config = server; */
+    api->hosts = &server->hosts;
 
     /* API plugins funcions */
 

plugins/auth/conf.c
@@ -146,7 +146,7 @@ int mk_auth_conf_init_users_list()
 
     /* vhost configuration */
     struct mk_list *head_hosts;
-    struct mk_list *hosts = &mk_api->config->hosts;
+    struct mk_list *hosts = mk_api->hosts;
     struct mk_list *head_sections;
     struct mk_vhost *entry_host;
     struct mk_rconf_section *section;